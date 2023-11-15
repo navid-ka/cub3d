4f072b6dc381590b93c1ae3f7b303cdd232db5f9
Bifrost <nkeyani-@student.42barcelona.com>
2023-11-15 20:28:55 +0100

    init and parser def

--- src/parser/structs_init.c
+++ src/parser/structs_init.c
@@ -1,24 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   structs_init.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bifrost <bifrost@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/14 12:24:47 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/11/15 20:27:53 by bifrost          ###   ########.fr       */
+/*   Created: 2023/11/15 20:14:03 by bifrost           #+#    #+#             */
+/*   Updated: 2023/11/15 20:27:01 by bifrost          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/cub3d.h"
+#include "../../include/cub3d.h"
 
-int main(int argc, char **argv)
+void    cub_init(t_cub *init, char **argv)
 {
-	t_cub	cub;
-
-	if (argc > 2)
-		return (0);
-	parser(&cub, argv + 1);
-	printf("%s\n", cub.path);
-	return (0);
+    init->path = ft_strdup(*argv);
 }
