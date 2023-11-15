084a111b95f90283436ba9febb89f1d2b64f74ba
Bifrost <nkeyani-@student.42barcelona.com>
2023-11-15 21:10:13 +0100

    argc handler

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: bifrost <bifrost@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 12:24:47 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/11/15 20:27:53 by bifrost          ###   ########.fr       */
+/*   Updated: 2023/11/15 21:08:26 by bifrost          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int main(int argc, char **argv)
 {
 	t_cub	cub;
 
-	if (argc > 2)
-		return (0);
+	if (argc > 2 || argc < 2)
+		return (printf("Error\nToo many arguments\n"));
 	parser(&cub, argv + 1);
 	printf("%s\n", cub.path);
 	return (0);
