36cee95bce96abcc21140a9bf2b0fab8d2e37def
Bifrost <nkeyani-@student.42barcelona.com>
2023-11-15 20:57:54 +0100

    init structs with all elements in the .cub map

--- src/map_renderer/draw_map.c
+++ src/map_renderer/draw_map.c
@@ -1,24 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   structs_init.c                                     :+:      :+:    :+:   */
+/*   draw_map.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bifrost <bifrost@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 20:14:03 by bifrost           #+#    #+#             */
-/*   Updated: 2023/11/15 20:46:08 by bifrost          ###   ########.fr       */
+/*   Updated: 2023/11/15 20:40:47 by bifrost          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
-
-void    cub_init(t_cub *init, char **argv)
-{
-	init->path = ft_strdup(*argv);
-	init->no = NULL;
-	init->so = NULL;
-	init->we = NULL;
-    init->ea = NULL;
-    init->f_color = NULL;
-    init->c_color = NULL;
-}
