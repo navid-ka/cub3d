45f70623a7f72c01a0cb458d66eea891381da5fd
Bifrost <nkeyani-@student.42barcelona.com>
2023-11-25 14:08:00 +0100

    fd integrity and orientation assignment

--- src/parser/fd_map_checker.c
+++ src/parser/fd_map_checker.c
@@ -6,12 +6,26 @@
 /*   By: nkeyani- <nkeyani-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 23:05:37 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/11/18 01:14:21 by nkeyani-         ###   ########.fr       */
+/*   Updated: 2023/11/25 14:02:23 by nkeyani-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
 
+
+void fd_error(t_cub *cub)
+{
+	if (cub->no)
+		free(cub->no);
+	if (cub->so)
+		free(cub->so);
+	if (cub->ea)
+		free(cub->ea);
+	if (cub->we)
+		free(cub->we);
+	ft_printf("Error\nTexture could not be loaded\n");
+	exit(1);
+}
 void    check_extension(t_cub *cub)
 {
 	char    *str;
@@ -27,35 +41,73 @@ void    check_extension(t_cub *cub)
 		exit(1);
 	}
 }
-char *fd_setter(char *line)
+char *fd_texture_setter(char *line)
 {
 	char	**opt;
 	char	*dup;
 	
 	opt = ft_split(line, ' ');
+	opt[1] = ft_strtrim(opt[1], "\n");
+	printf("setter: %s \n", opt[1]);
 	dup = ft_strdup(opt[1]);
 	free_tab(opt);
 	return(dup);
 }
-char *fd_getter(char *line, char *value)
+int	fd_texture_checker(char *line)
 {
-	
-}
+	int	fd;
 
+	fd = open(line, O_RDONLY);
+	printf("line: [%s] fd: [%d]\n", line, fd);
+	if (fd == -1)
+		return (1);
+	close(fd);
+	return (0);
+}
+int	orientation_checker(t_cub *cub, char *line, int *flag)
+{
+	if (line && ft_strnstr(line, "NO", 3))
+	{
+		cub->no = fd_texture_setter(line);
+		if (fd_texture_checker(cub->no))
+			flag += 1;
+	}
+	if (line && ft_strnstr(line, "SO", 3))
+	{
+		cub->so = fd_texture_setter(line);
+		if (fd_texture_checker(cub->so))
+			flag += 1;
+	}
+	if (line && ft_strnstr(line, "WE", 3))
+	{
+		cub->we = fd_texture_setter(line);
+		if (fd_texture_checker(cub->we))
+			flag += 1;
+	}
+	if (line && ft_strnstr(line, "EA", 3))
+	{
+		cub->ea = fd_texture_setter(line);
+		if (fd_texture_checker(cub->ea))
+			flag += 1;
+	}
+	return (*flag);
+}
 void    check_fd_integrity(t_cub *cub)
 {
 	int			fd;
 	char		*line;
+	int			flag;
 
 	fd = open(cub->path, O_RDONLY);
 	printf("%d\n", fd);
 	line = ft_strdup("");
 	while (line)
 	{
+		flag = 0;
 		line = get_next_line(fd);
-		if (line && ft_strnstr(line, "SO", 3))
-			cub->so = fd_setter(line);
+		if (orientation_checker(cub, line, &flag))
+			fd_error(cub);
 	}
-	printf("%s", cub->so);
+	printf("%s\n", cub->so);
 	close(fd);
 }
