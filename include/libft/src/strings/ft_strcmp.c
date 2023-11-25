073eddf1afac2dfa0f64d1b61e49c90f7a9ec4b5
Bifrost <nkeyani-@student.42barcelona.com>
2023-11-25 14:08:00 +0100

    fd integrity and orientation assignment

--- include/libft/src/strings/ft_strcmp.c
+++ include/libft/src/strings/ft_strcmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nkeyani- < nkeyani-@student.42barcelona    +#+  +:+       +#+        */
+/*   By: nkeyani- <nkeyani-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 17:28:06 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/08/03 17:40:57 by nkeyani-         ###   ########.fr       */
+/*   Updated: 2023/11/25 13:30:32 by nkeyani-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ int	ft_strcmp(char *s1, char *s2)
 {
 	size_t	i;
 
+	if (!s1 || !s2)
+		return (1);
 	i = 0;
-	while (s1[i] == s2[i] && s1[i] && s2[i])
+	while (s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
 	return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 }
