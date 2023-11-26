5d0c011806265d20433c953998c5470741fc294c
Bifrost <nkeyani-@student.42barcelona.com>
2023-11-26 13:17:43 +0100

    added cub_atoi to check if the c or f is > 255

--- src/parser/utils.c
+++ src/parser/utils.c
@@ -6,11 +6,29 @@
 /*   By: bifrost <bifrost@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 23:05:37 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/11/26 12:15:03 by bifrost          ###   ########.fr       */
+/*   Updated: 2023/11/26 13:14:37 by bifrost          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
+
+int	cub_atoi(const char *str)
+{
+    int num = 0;
+    int i = 0;
+
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        num = num * 10 + (str[i] - '0');
+        if (num > 255) 
+		{
+            ft_printf("Error\nNumber out of range (0-255)\n");
+            exit (1); //TODO; aqui puede que tengamos que hacer garbage collection
+        }
+        i++;
+    }
+    return (num);
+}
 
 char *fd_setter(char *line)
 {
