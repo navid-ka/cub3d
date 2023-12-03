0dd3d47a8d12c9273b1bf337283ef53bfb15a0b7
Bifrost <nkeyani-@student.42barcelona.com>
2023-12-03 12:40:38 +0100

    fixed map read with newlines in the middle TODO: check if line only has spaces

--- src/garbage_collector/garbage.c
+++ src/garbage_collector/garbage.c
@@ -6,7 +6,7 @@
 /*   By: bifrost <bifrost@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 23:05:37 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/11/26 12:14:35 by bifrost          ###   ########.fr       */
+/*   Updated: 2023/12/03 12:34:35 by bifrost          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,16 +31,65 @@ void	free_tab(char **args)
 	}
 }
 
-void fd_error(t_cub *cub)
+void free_null(char **ptr)
 {
-	if (cub->no)
-		free(cub->no);
-	if (cub->so)
-		free(cub->so);
-	if (cub->ea)
-		free(cub->ea);
-	if (cub->we)
-		free(cub->we);
-	ft_printf("Error\nTexture could not be loaded\n");
-	exit(1);
+    free(*ptr);
+    *ptr = NULL;
+}
+
+void free_textures(t_cub *cub)
+{
+	if (cub->path)
+        free_null(&cub->path);
+    if (cub->no)
+        free_null(&cub->no);
+    if (cub->so)
+        free_null(&cub->so);
+    if (cub->ea)
+        free_null(&cub->ea);
+    if (cub->we)
+		free_null(&cub->we);
+	if (cub->tmp)
+		free_null(&cub->tmp);
+}
+
+void fd_error_1(t_cub *cub)
+{
+    free_textures(cub);
+    ft_printf("Error\nTexture could not be loaded\n");
+    exit(1);
+}
+
+void fd_error_2(t_cub *cub)
+{
+    free_textures(cub);
+    if (cub->map)
+        free_tab(cub->map);
+    printf("Error\nMap not valid\n");
+    exit(1);
+}
+
+void	fd_error(t_cub *cub, int err)
+{
+    if (err == 1)
+        fd_error_1(cub);
+    else if (err == 2)
+        fd_error_2(cub);
+}
+
+void garbage_collector(t_cub *cub)
+{
+    if (cub->no)
+        free_null(&cub->no);
+    if (cub->so)
+        free_null(&cub->so);
+    if (cub->ea)
+        free_null(&cub->ea);
+    if (cub->we)
+        free_null(&cub->we);
+    if (cub->path)
+        free_null(&cub->path);
+    if (cub->map)
+        free_tab(cub->map);
+    printf("garbage collector\n");
 }
