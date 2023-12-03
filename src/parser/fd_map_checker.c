7b2ef99dd1556dcf0775062ecb5dcc08b9c3f7a7
Bifrost <nkeyani-@student.42barcelona.com>
2023-12-03 18:52:31 +0100

    fix: logic in game read

--- src/parser/fd_map_checker.c
+++ src/parser/fd_map_checker.c
@@ -6,7 +6,7 @@
 /*   By: bifrost <bifrost@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 23:05:37 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/12/03 12:39:18 by bifrost          ###   ########.fr       */
+/*   Updated: 2023/12/03 18:51:53 by bifrost          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,14 +44,11 @@ int	is_map(char *line)
     return (0);
 }
 
-int read_map(t_cub *cub, char *line)
+void    read_map(t_cub *cub, char *line)
 {
-    if (line && line[0] == '\n')
-        return (1);
     cub->new_tmp = ft_strjoin(cub->tmp, line);
     free(cub->tmp);
     cub->tmp = cub->new_tmp;
-    return (0);
 }
 
 int	check_map_fd(t_cub *cub, int map_started)
@@ -68,12 +65,10 @@ int	check_map_fd(t_cub *cub, int map_started)
     {
 		free(line);
 		line = get_next_line(fd);
-        if (line && is_map(line)) // TODO: check if map line has only spaces
+        if (line && is_map(line))
         {
             map_started = 1;
-            cub->new_tmp = ft_strjoin(cub->tmp, line);
-            free(cub->tmp);
-            cub->tmp = cub->new_tmp;
+            read_map(cub, line);
         }
         if (line && map_started && line[0] == '\n' && line[1] == '\0')
             cub->err = 2;
@@ -107,6 +102,5 @@ void    check_fd_integrity(t_cub *cub)
 		fd_error(cub, cub->err);
 	if (!cub->err)
 		cub->map = ft_split(cub->tmp, '\n');
-	free(cub->new_tmp);
 	fd_print(cub);
 }
