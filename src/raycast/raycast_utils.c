1185c648ccc02cf6ec356010bd71b7318b7bd7b3
plinscho <paul.linschoten@gmail.com>
2023-12-06 00:03:12 +0100

    Made raycast files and headers

--- src/raycast/raycast_utils.c
+++ src/raycast/raycast_utils.c
@@ -1,28 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   raycast_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: plinscho <plinscho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/14 12:24:47 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/12/06 00:00:15 by plinscho         ###   ########.fr       */
+/*   Created: 2023/12/05 23:33:30 by plinscho          #+#    #+#             */
+/*   Updated: 2023/12/05 23:33:31 by plinscho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/cub3d.h"
-
-int main(int argc, char **argv)
-{
-	t_cub		cub;
-	t_player	p1;
-
-	(void)p1;
-	if (argc > 2 || argc < 2)
-		return (printf(ARGC));
-	
-	parser(&cub, argv + 1);
-    ft_cast_rays(&p1, cub.map);
-	garbage_collector(&cub);
-	return (0);
-}
