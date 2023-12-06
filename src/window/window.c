203193dc1c9b0c20c14984bd2876c6e88a9c188f
Bifrost <nkeyani-@student.42barcelona.com>
2023-12-06 16:30:46 +0100

    ESC not working on arch

--- src/window/window.c
+++ src/window/window.c
@@ -6,15 +6,30 @@
 /*   By: bifrost <bifrost@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 15:19:26 by bifrost           #+#    #+#             */
-/*   Updated: 2023/12/06 15:28:57 by bifrost          ###   ########.fr       */
+/*   Updated: 2023/12/06 16:28:01 by bifrost          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
 
+int window_destroy(t_mlx *window)
+{
+    mlx_destroy_window(window->mlx, window->win);
+    exit(0);
+}
+int window_input(int keycode, t_mlx *window)
+{
+    printf("Keycode: %d\n", keycode);
+    if (keycode == KEY_ESC)
+        window_destroy(window);
+    return (0);
+}
+
 void    window_init(t_mlx *window)
 {
     window->mlx = mlx_init();
-    window->win = mlx_new_window(window->mlx, 640, 480, "Hello world!");
+    window->win = mlx_new_window(window->mlx, 640, 480, "Cub3D");
+	mlx_hook(window->win, 17, 0, &window_destroy, &window);
+    mlx_hook(window->win, 2, 0, &window_input, &window);
     mlx_loop(window->mlx);
 }
