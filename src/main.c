d19b4bf7a5d230b80d33be1f08d333fe15dd5104
Bifrost <nkeyani-@student.42barcelona.com>
2023-12-08 13:53:59 +0100

    fixed orientations and strchr removed 2from valid chars

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: bifrost <bifrost@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 12:24:47 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/12/06 18:20:44 by bifrost          ###   ########.fr       */
+/*   Updated: 2023/12/06 23:18:33 by bifrost          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 int main(int argc, char **argv)
 {
-	t_cub		cub;
-	t_map		map;
-	t_player	p1;
-	t_mlx		window;
+	t_cub cub;
+	t_map map;
+	t_player p1;
+	t_mlx window;
 
 	(void)p1;
 	if (argc > 2 || argc < 2)
 		return (printf(ARGC));
 	fd_parser(&cub, &map, argv + 1);
-    ft_cast_rays(&p1, cub.map);
+	ft_cast_rays(&p1, cub.map);
 	window.cub = &cub;
 	window.map = &map;
 	mlx_window(&window);
