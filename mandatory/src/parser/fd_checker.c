6b6f8fdd47343849a314bcd5320c767d9d71cce5
Bifrost <nkeyani-@student.42barcelona.com>
2023-12-29 10:48:54 +0100

    Redone all folders added bonus, implemented state manager for bonus

---
+++ mandatory/src/parser/fd_checker.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fd_checker.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plinscho <plinscho@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/17 23:05:37 by nkeyani-          #+#    #+#             */
+/*   Updated: 2023/12/10 18:10:08 by plinscho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/cub3d.h"
+
+void    fd_check_extension(t_cub *cub)
+{
+	char    *str;
+	int     len;
+
+	str = cub->path;
+	len = ft_strlen(str);
+	printf("str: %s\n", str);
+	if (!(str[len - 1] == 'b' && str[len - 2] == 'u' && str[len - 3] == 'c'
+			&& str[len - 4] == '.'))
+	{
+		free(str);
+		ft_printf("Error\nIncorrect file extension\n");
+		exit(1);
+	}
+}
+
+int	is_map(char *line)
+{
+    int i;
+
+    i = 0;
+    while (line[i])
+    {
+        if (line[i] == '1' || line[i] == '0' || line[i] == ' ')
+            return (1);
+        else if (line[i] != ' ')
+            return (0);
+        i++;
+    }
+    return (0);
+}
+
+void    read_map(t_cub *cub, char *line)
+{
+    cub->new_tmp = ft_strjoin(cub->tmp, line);
+    free(cub->tmp);
+    cub->tmp = cub->new_tmp;
+}
+
+int	fd_check_map(t_cub *cub, int map_started)
+{
+    int     fd;
+    char    *line;
+
+    fd = open_path(cub);
+    cub->tmp = ft_strdup("");
+    line = ft_strdup("");
+    while (line)
+    {
+		free(line);
+		line = get_next_line(fd);
+        if (line && is_map(line))
+        {
+            map_started = 1;
+            read_map(cub, line);
+        }
+        if (line && map_started && line[0] == '\n' && line[1] == '\0')
+            cub->err = 2;
+    }
+    close(fd);
+    return (cub->err);
+}
+
+void    fd_check_integrity(t_cub *cub, t_map *map)
+{
+	int		fd;
+	char	*line;
+	int		flag;
+
+    fd = open_path(cub);
+	line = ft_strdup("");
+	while (line)
+	{
+		free(line);
+		line = get_next_line(fd);
+		flag = 0;
+		if (line && !is_map(line) \
+			&& (fd_orientation_checker(cub, line, &flag) \
+			|| fd_color_checker(cub, line, &flag)))
+			cub->err = 1;
+	}
+	close(fd);
+	if (cub->count < 6)
+		cub->err = 1;
+	fd_check_map(cub, 0);
+	if (cub->err)
+		fd_error(cub, cub->err);
+	cub->map = ft_split(cub->tmp, '\n');
+	map->map = ft_split(cub->tmp, '\n');
+	free(cub->tmp);
+	fd_print(cub);
+}
---
+++ mandatory/src/parser/structs_init.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structs_init.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bifrost <bifrost@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/15 20:14:03 by bifrost           #+#    #+#             */
+/*   Updated: 2023/12/12 01:21:48 by bifrost          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/cub3d.h"
+
+void    cub_init(t_cub *init, char **argv)
+{
+	init->path = ft_strdup(*argv);
+	init->no = NULL;
+	init->so = NULL;
+	init->we = NULL;
+    init->ea = NULL;
+	init->f_color = NULL;
+	init->c_color = NULL;
+	init->map = NULL;
+	init->tmp = NULL;
+	init->new_tmp = NULL;
+	init->err = 0;
+	init->count = 0;
+	ft_memset(&init->floor, 0, sizeof(t_color));
+	ft_memset(&init->ceiling, 0, sizeof(t_color));
+}
+void	map_init(t_map *map)
+{
+	map->width = 0;
+	map->height = 0;
+	map->map = NULL;
+	map->orientation = 0;
+}
+
+void	player_init(t_player *player)
+{
+	player->pos_x = 0;
+	player->pos_y = 0;
+	player->dir_x = 0;
+	player->dir_y = 0;
+	player->fov = 0;
+}
+
+
+void    window_init(t_mlx *window)
+{
+	window->mlx_p = NULL;
+	window->win = NULL;
+}
