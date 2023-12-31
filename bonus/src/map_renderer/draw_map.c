badbbc09f4915b79aefc65d31da1fed65e0f2525
Bifrost <nkeyani-@student.42barcelona.com>
2023-12-31 20:00:53 +0100

    added portrait

--- bonus/src/map_renderer/draw_map.c
+++ bonus/src/map_renderer/draw_map.c
@@ -6,7 +6,7 @@
 /*   By: bifrost <bifrost@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 14:00:44 by plinscho          #+#    #+#             */
-/*   Updated: 2023/12/28 17:56:07 by bifrost          ###   ########.fr       */
+/*   Updated: 2023/12/31 15:38:24 by bifrost          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,9 @@ void	sl_image_init(t_mlx *g)
 	w = 32;
 	int w_w = 1280;
 	int w_h = 720;
-	g->img = malloc(sizeof(t_img) * (4 + 1));
+	int p_w = 128;
+	int p_h = 128;
+	g->img = malloc(sizeof(t_img) * (5 + 1));
 	if (!g->img)
 	{
 		printf("Error\n[errno: %d] Malloc failed\n", errno);
@@ -33,7 +35,87 @@ void	sl_image_init(t_mlx *g)
 	g->img[1].img = mlx_xpm_file_to_image(g->mlx_p, "textures/map/path.xpm", &w, &h);
 	g->img[2].img = mlx_xpm_file_to_image(g->mlx_p, "textures/map/clear.xpm", &w, &h);
 	g->img[3].img = mlx_xpm_file_to_image(g->mlx_p, "textures/map/start.xpm", &w_w, &w_h);
+	g->img[4].img = mlx_xpm_file_to_image(g->mlx_p, "textures/player/portrait.xpm", &p_w, &p_h);
+	g->img[4].addr = mlx_get_data_addr(g->img[4].img, &g->img[4].bpp, &g->img[4].line_len, &g->img[4].endian);
 }
+
+void	img_pix_put(t_img *img, int x, int y, int color)
+{
+    char    *pixel;
+
+    pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
+    *(int *)pixel = color;
+}
+
+t_img	*resize_image(t_mlx *g, t_img *img, int nw, int nh)
+{
+    static t_position	od = {128, 128};
+    t_img				*ri;
+    int					c;
+    t_position			p = {0, 0};
+    t_position			op;
+
+    ri = malloc(sizeof(t_img));
+    ri->img = mlx_new_image(g->mlx_p, nw, nh);
+    ri->addr = mlx_get_data_addr(ri->img, &ri->bpp, &ri->line_len, &ri->endian);
+    while (p.y < nh)
+    {
+        p.x = 0;
+        while (p.x < nw)
+        {
+            op.x = p.x * od.x / nw;
+            op.y = p.y * od.y / nh;
+            c = ((int *)img->addr)[op.y * od.x + op.x];
+            img_pix_put(ri, p.x, p.y, c);
+            p.x++;
+        }
+        p.y++;
+    }
+    return (ri);
+}
+
+void draw_resized_img(t_mlx *g, t_img *img, int pos[2], int size[2])
+{
+    static t_img *resized_img;
+	static int flag = 0;
+	if (!flag)
+	{
+		resized_img = resize_image(g, img, size[0], size[1]);
+		flag = 1;
+	}
+    mlx_put_image_to_window(g->mlx_p, g->win, resized_img->img, pos[0], pos[1]);
+}
+
+/*
+t_img *double_bpp_image(t_mlx *g, t_img *img, int width, int height)
+{
+	t_img *doubled_img = malloc(sizeof(t_img));
+	doubled_img->img = mlx_new_image(g->mlx_p, width, height);
+	doubled_img->addr = mlx_get_data_addr(doubled_img->img, &doubled_img->bpp, &doubled_img->line_length, &doubled_img->endian);
+	int x = 0;
+	int y = 0;
+	int color;
+	while (y < height)
+	{
+		while (x < width)
+		{
+			color = ((int *)img->addr)[(y * img->line_length + x * (img->bpp / 8)) / 4];
+			img_pix_put(doubled_img, x, y, color);
+			x++;
+		}
+		x = 0;
+		y++;
+	}
+	return doubled_img;
+}
+
+
+void draw_doubled_image(t_mlx *g, t_img *img, int pos_x, int pos_y)
+{
+	t_img *doubled_img = double_bpp_image(g, img, 64, 64);
+
+	mlx_put_image_to_window(g->mlx_p, g->win, doubled_img->img, pos_x, pos_y);
+}*/
 /*
 static void print_map(t_game *game)
 {
@@ -59,11 +141,11 @@ void clear_player(t_game *game)
 
 void draw_player(t_game *game)
 {
-    // Get the player's position in the map
+	// Get the player's position in the map
 	int y = (int)game->player_s->pos_y * 32;
 	int x = (int)game->player_s->pos_x * 32;
 
-    // Draw the player tile on the screen
+	// Draw the player tile on the screen
 //    printf("Player tile: %c\n", game->map_s->map[player_row][player_col]);
-    mlx_put_image_to_window(game->mlx_s->mlx_p, game->mlx_s->win, game->mlx_s->img[0].img, x, y);
+	mlx_put_image_to_window(game->mlx_s->mlx_p, game->mlx_s->win, game->mlx_s->img[0].img, x, y);
 }
