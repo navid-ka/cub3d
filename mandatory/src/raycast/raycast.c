54110d111a3f75f341781f7a9638e3e097b34454
plinscho <plinscho@plinscho>
2024-01-04 21:55:48 +0100

    distance improved

--- mandatory/src/raycast/raycast.c
+++ mandatory/src/raycast/raycast.c
@@ -14,19 +14,23 @@
 int	check_wall(t_game *game, double x0, double y0);
 
 // to start, we need the player position and the player direction
-void    draw_line(t_game *game, double x0, double y0, double angle)
+double    draw_line(t_game *game, double x0, double y0, double angle)
 {
+	double	x_inc;
+	double	y_inc;
+
+	x_inc = x0;
+	y_inc = y0;
 	for (int i = 0 ; i < RENDER_DIST ; i++)
 	{
 		// check if it hits a wall. Will need the map.
 		mlx_pixel_put(game->mlx_s->mlx_p, game->mlx_s->win, (x0), (y0), 0x00AAFF00);
 		x0 += 1 * cos(angle);
 		y0 += 1 * sin(angle);
-		if (check_wall
-		(game, x0, y0))
-			break ;
+		if (check_wall(game, x0, y0))
+			return (sqrt((pow(x_inc - x0, 2)) + pow(y_inc - y0, 2)));
 	}
-	return ;
+	return (RENDER_DIST);
 }
 
 int	check_wall(t_game *game, double x0, double y0)
@@ -43,17 +47,28 @@ int	check_wall(t_game *game, double x0, double y0)
 
 void    raycast(t_game *game)
 {
-	double  ray_angle;
+	double  orientation;
+	double	distance;
 	double	x0;
 	double	y0;
 	
 	y0 = game->player_s->pos_y * 32 + 16;
 	x0 = game->player_s->pos_x * 32 + 16;
-	ray_angle = game->player_s->angle;
-	while (ray_angle < game->player_s->fov + game->player_s->angle)
+	orientation = game->player_s->angle;
+	while (orientation == game->player_s->angle)//< game->player_s->fov + game->player_s->angle)
 	{
-		draw_line(game, x0, y0, ray_angle);
-		ray_angle += 0.020;
+		distance = draw_line(game, x0, y0, orientation) - 16;
+		distance /= 32;
+		printf("distance: %f squares\n", distance);
+		double px = x0 + ((int)distance);
+		double py = y0 + ((int)distance);
+		printf("Looking at [%f, %f]\n", px, py);
+//		draw the points in the screen using the distance.
+/*
+		(distance > 0) 
+		draw_camera()		
+*/		
+		orientation += ROTATE_SPEED;
 	}
 	return ;
 }
