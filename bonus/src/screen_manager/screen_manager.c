eab0ec0bef34ba37d6112d8872f71224ada4625d
Bifrost <nkeyani-@student.42barcelona.com>
2024-01-04 12:25:19 +0100

    minimap working again

--- bonus/src/screen_manager/screen_manager.c
+++ bonus/src/screen_manager/screen_manager.c
@@ -6,7 +6,7 @@
 /*   By: bifrost <bifrost@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 17:08:51 by bifrost           #+#    #+#             */
-/*   Updated: 2024/01/03 11:47:10 by bifrost          ###   ########.fr       */
+/*   Updated: 2024/01/04 11:43:38 by bifrost          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int	title_menu(t_game *game)
 int	pause_menu(t_game *game)
 {
 	//put_img_to_img(game->mlx_s->buffer, &game->mlx_s->img[4], 0, 0);
-	draw_minimap(game);
+	(void)game;
 	return (PAUSE);
 }
 int	screen_manager(t_game *g, enum e_state state)
