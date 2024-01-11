cbc20f15518ab73e7ce9387aabef7de1abb6312e
Bifrost <nkeyani-@student.42barcelona.com>
2024-01-11 15:31:00 +0100

    fight v0.9

--- bonus/src/combat/enemies_loader.c
+++ bonus/src/combat/enemies_loader.c
@@ -6,7 +6,7 @@
 /*   By: bifrost <bifrost@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 04:01:07 by bifrost           #+#    #+#             */
-/*   Updated: 2024/01/09 20:59:29 by bifrost          ###   ########.fr       */
+/*   Updated: 2024/01/11 15:28:01 by bifrost          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void load_enemy_img(t_mlx *g)
 {
     int h = 64;
     int w = 64;
+    int bh = 32;
+    int bw = 32;
 
     g->enemy = malloc(sizeof(t_img) * (2 + 1));
     if (!g->enemy)
@@ -28,5 +30,10 @@ void load_enemy_img(t_mlx *g)
     g->enemy[0].width = w;
     g->enemy[0].height = h;
     g->enemy[0] = *resize_image(g, &g->enemy[0], 256, 256);
+    g->enemy[1].img = mlx_xpm_file_to_image(g->mlx_p, "textures/enemies/enemy1.xpm", &bw, &bh);
+    g->enemy[1].addr = mlx_get_data_addr(g->enemy[1].img, &g->enemy[1].bpp, &g->enemy[1].line_len, &g->enemy[1].endian);
+    g->enemy[1].width = bw;
+    g->enemy[1].height = bh;
+    g->enemy[1] = *resize_image(g, &g->enemy[1], 512, 512);
 }
 
